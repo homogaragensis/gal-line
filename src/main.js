4b02e4a58fde2c5faefd468be88e6e2f1ad43955
Tiuna Angelini <tiuna.angelini@gmail.com>
2026-09-13 20:09:56 +0200

    fixed the canvas resolution and display size

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 (() => {
-  alert("hello, world");
+  const canvas = document.getElementById("renderCanvas");
+  canvas.width = 1280;
+  canvas.width = 680;
+
+  document.documentElement.setProperty(
+    "--viewport-aspect", canvas.width / canvas.height
+  );
 })();
