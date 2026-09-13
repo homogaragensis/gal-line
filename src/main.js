d93e47e2ed0f8f8a5749a3967259109ff0429a66
Tiuna Angelini <tiuna.angelini@gmail.com>
2026-09-13 20:15:14 +0200

    finally fixed the canvas sizing and set it to a really fun color

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 (() => {
   const canvas = document.getElementById("renderCanvas");
   canvas.width = 1280;
-  canvas.width = 680;
+  canvas.height = 680;
 
   document.documentElement.style.setProperty(
     "--viewport-aspect", canvas.width / canvas.height
   );
+
+  const ctx = canvas.getContext("2d");
+  ctx.fillStyle = "#000000";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 })();
