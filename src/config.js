7863547ba5b1a7c31169550b4dff88a02ac22888
Tiuna Angelini <tiuna.angelini@gmail.com>
2026-09-13 20:24:59 +0200

    Moved constants to a configuration file

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const Config = {
+  viewport: {
+    width: 1280,
+    height: 680,
+    clearColor: "#9999ff"
+  },
+};
--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
+import { Config } from "./config.js";
+
 (() => {
   const canvas = document.getElementById("renderCanvas");
-  canvas.width = 1280;
-  canvas.height = 680;
+  canvas.width = Config.viewport.width;
+  canvas.height = Config.viewport.height;
 
   document.documentElement.style.setProperty(
     "--viewport-aspect", canvas.width / canvas.height
   );
 
   const ctx = canvas.getContext("2d");
-  ctx.fillStyle = "#000000";
+  ctx.fillStyle = Config.viewport.clearColor;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 })();
